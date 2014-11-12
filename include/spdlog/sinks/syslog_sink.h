b0b716fc30bc9ca4fda4da78c85c751ee921afc2
Paul <pauljnospambaxter@hotnospammail.com>
2014-11-12 13:06:26 +0000

    Fixup syslog on windows to compile - logger does nothing

--- include/spdlog/sinks/syslog_sink.h
+++ include/spdlog/sinks/syslog_sink.h
@@ -24,10 +24,20 @@
 
 #pragma once
 
-#ifdef __linux__
-
 #include <string>
+#ifdef __linux__
 #include <syslog.h>
+#else
+	inline void syslog(int /*prio*/, const char */*fmt*/, ...) {}	// TODO Windows Syslog not supported
+	#define LOG_EMERG       0       /* system is unusable */
+	#define LOG_ALERT       1       /* action must be taken immediately */
+	#define LOG_CRIT        2       /* critical conditions */
+	#define LOG_ERR         3       /* error conditions */
+	#define LOG_WARNING     4       /* warning conditions */
+	#define LOG_NOTICE      5       /* normal but significant condition */
+	#define LOG_INFO        6       /* informational */
+	#define LOG_DEBUG       7       /* debug-level messages */
+#endif
 #include "./sink.h"
 #include "../common.h"
 #include "../details/log_msg.h"
@@ -84,5 +94,3 @@ namespace spdlog
     };
   }
 }
-
-#endif
