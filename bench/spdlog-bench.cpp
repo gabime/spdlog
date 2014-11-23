e75b082b6f64a20bc6b702c5de3dde8221944d7f
gabime <gmelman1@gmail.com>
2014-11-23 02:06:42 +0200

    bench

--- bench/spdlog-bench.cpp
+++ bench/spdlog-bench.cpp
@@ -7,7 +7,7 @@ int main(int, char* [])
     int howmany = 1000000;
     namespace spd = spdlog;
     ///Create a file rotating logger with 5mb size max and 3 rotated files
-    auto logger = spd::rotating_logger_mt("file_logger", "logs/spd-sample", 100 *1024 * 1024 , 5);
+    auto logger = spd::rotating_logger_mt("file_logger", "logs/spd-sample", 10 *1024 * 1024 , 5);
 
     logger->set_pattern("[%Y-%b-%d %T.%e]: %v");
     for(int i  = 0 ; i < howmany; ++i)
