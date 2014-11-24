0926a6e2c358cd959bbb666b4a2842d4dd67d904
gabime <gmelman1@gmail.com>
2014-11-24 02:15:17 +0200

    Added async to benchmarks

--- bench/run_all.sh
+++ bench/run_all.sh
@@ -3,27 +3,36 @@ echo "Running benchmakrs (all with 1000,000 writes to the logs folder"
 echo
 echo "boost-bench (single thread).."
 time ./boost-bench
-echo ==================================
+echo
+echo
 sleep 3
 echo "glog-bench (single thread).."
 time ./glog-bench
-echo ==================================
+echo
+echo
 sleep 3
 echo "spdlog-bench (single thread)"
 time ./spdlog-bench
-echo ==================================
+echo
+echo
 sleep 3
 echo "boost-bench-mt (10 threads, single logger)"..
 time ./boost-bench-mt
-echo ==================================
+echo
+echo
 sleep 3
 echo "glog-bench-mt (10 threads, single logger)"..
 time ./glog-bench-mt
-echo ==================================
+echo
+echo
 sleep 3
 echo "spdlog-bench-mt (10 threads, single logger)"..
 time ./spdlog-bench-mt
-echo ==================================
 echo
+echo
+sleep 3
+echo "spdlog-bench-mt-async (10 threads, single logger)"..
+time ./spdlog-bench-mt-async
+
 
 
