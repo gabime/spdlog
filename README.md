c948f15822331499cc6caea0ff5ca5930393348e
gabime <gmelman1@gmail.com>
2014-11-24 23:38:58 +0200

    updated benchmarks

--- README.md
+++ README.md
@@ -38,9 +38,9 @@ Just copy the files to your build tree and use a C++11 compiler
 Below are some [benchmarks](bench) comparing the time needed to log 1,000,000 lines to file under Ubuntu 64 bit, Intel i7-4770 CPU @ 3.40GHz:
 
 |threads|boost log|glog|g2log|spdlog|spdlog<sup>async mode</sup>|
-|-------|:-------:|:-----:|------:|------:|
-|1|4.430s|1.115s|3.227s|0.940s|1.574s
-|10|13.985s|2.027s|3.476s|3.822s|1.972
+|-------|:-------:|:-----:|------:|------:|------:|
+|1|4.779s|1.109s|3.155s|0.947s|1.455s
+|10|15.151ss|3.546s|3.500s|1.549s|2.040s|
 
 
 
--- bench/run_all.sh
+++ bench/run_all.sh
@@ -3,53 +3,61 @@ echo "Running benchmakrs (all with 1000,000 writes to the logs folder"
 echo
 echo "boost-bench (single thread).."
 time ./boost-bench
+rm logs/*
 echo
 echo
-sleep 3
+sleep 5
 
 echo "glog-bench (single thread).."
 time ./glog-bench
+rm logs/*
 echo
 echo
-sleep 3
+sleep 5
 
 echo "g2log-bench (single thread).."
 time ./g2log-bench
+rm logs/*
 echo
 echo
-sleep 3
+sleep 5
 
 echo "spdlog-bench (single thread)"
 time ./spdlog-bench
+rm logs/*
 echo
 echo
-sleep 3
+sleep 5
 echo "------------------------------------"
 echo "Multithreaded benchmarks.."
 echo "------------------------------------"
 echo "boost-bench-mt (10 threads, single logger)"..
 time ./boost-bench-mt
+rm logs/*
 echo
 echo
-sleep 3
+sleep 5
 
 echo "glog-bench-mt (10 threads, single logger)"..
 time ./glog-bench-mt
+rm logs/*
 echo
 echo
-sleep 3
+sleep 5
 
 echo "g2log-bench-mt (10 threads, single logger)"..
 time ./g2log-bench-mt
+rm logs/*
 echo
 echo
-sleep 3
+sleep 5
 
 echo "spdlog-bench-mt (10 threads, single logger)"..
 time ./spdlog-bench-mt
+rm logs/*
 echo
 echo
-sleep 3
+sleep 5
 
 echo "------------------------------------"
 echo "Async  benchmarks.."
@@ -57,9 +65,10 @@ echo "------------------------------------"
 
 echo "spdlog-bench-async (single thread)"..
 time ./spdlog-bench-async
+rm logs/*
 echo
 echo
-sleep 3
+sleep 5
 
 echo "spdlog-bench-mt-async (10 threads, single logger)"..
 time ./spdlog-bench-mt-async
