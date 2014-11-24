135d0e221caab05fef310dc546d70088d2370757
gabime <gmelman1@gmail.com>
2014-11-24 02:44:45 +0200

    g2log benchmarks

---
+++ bench/g2log-bench-mt.cpp
@@ -0,0 +1,49 @@
+#include <thread>
+#include <vector>
+#include <atomic>
+#include <iostream>
+
+#include "g2logworker.h"
+#include "g2log.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+    int thread_count = 10;
+    if(argc > 1)
+        thread_count = atoi(argv[1]);
+
+    int howmany = 1000000;
+
+    g2LogWorker g2log(argv[0], "logs");
+    g2::initializeLogging(&g2log);
+      
+
+    std::atomic<int > msg_counter {0};
+    vector<thread> threads;
+
+    for (int t = 0; t < thread_count; ++t)
+    {
+        threads.push_back(std::thread([&]()
+        {
+            while (true)
+            {
+                int counter = ++msg_counter;
+                if (counter > howmany) break;
+                LOG(INFO) << "glog message #" << counter << ": This is some text for your pleasure";
+            }
+        }));
+    }
+
+
+    for(auto &t:threads)
+    {
+        t.join();
+    };
+
+
+
+    return 0;
+}
--- bench/run_all.sh
+++ bench/run_all.sh
@@ -6,31 +6,49 @@ time ./boost-bench
 echo
 echo
 sleep 3
+
 echo "glog-bench (single thread).."
 time ./glog-bench
 echo
 echo
 sleep 3
+
+echo "g2log-bench (single thread).."
+time ./g2log-bench
+echo
+echo
+sleep 3
+
 echo "spdlog-bench (single thread)"
 time ./spdlog-bench
 echo
 echo
 sleep 3
+
 echo "boost-bench-mt (10 threads, single logger)"..
 time ./boost-bench-mt
 echo
 echo
 sleep 3
+
 echo "glog-bench-mt (10 threads, single logger)"..
 time ./glog-bench-mt
 echo
 echo
 sleep 3
+
+echo "g2log-bench-mt (10 threads, single logger)"..
+time ./g2log-bench-mt
+echo
+echo
+sleep 3
+
 echo "spdlog-bench-mt (10 threads, single logger)"..
 time ./spdlog-bench-mt
 echo
 echo
 sleep 3
+
 echo "spdlog-bench-mt-async (10 threads, single logger)"..
 time ./spdlog-bench-mt-async
 
