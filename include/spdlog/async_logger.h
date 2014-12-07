4dc75adf9bdb0f5ea748745c1149789cce9ac4fe
gabi <galim120@bezeqint.net>
2014-12-07 04:33:01 +0200

    async_logger comments

--- include/spdlog/async_logger.h
+++ include/spdlog/async_logger.h
@@ -24,12 +24,14 @@
 
 #pragma once
 
-// Very fast asynchronous logger (millions of logs per second on aregular desktop)
+// Very fast asynchronous logger (millions of logs per second on an average desktop)
 // Uses pre allocated lockfree queue for maximum throughput even under large number of threads.
+// Creates a single back thread to pop messages from the queue and log them.
+//
 // Upon each log write the logger:
 //    1. Checks if its log level is enough to log the message
-//    2. Push a new copy of the message to a queue or block the caller until space is available
-//    3. will throw spdlog_ex upon exceptions
+//    2. Push a new copy of the message to a queue (or block the caller until space is available in the queue)
+//    3. will throw spdlog_ex upon log exceptions
 // Upong destruction, logs all remaining messages in the queue before destructing..
 
 #include <chrono>
