489304a9532a861b6a68b72ba6f0d6c66a14e0d7
gabime <gmelman1@gmail.com>
2015-01-15 10:48:52 +0200

    style

--- include/spdlog/async_logger.h
+++ include/spdlog/async_logger.h
@@ -53,9 +53,24 @@ class async_logger :public logger
 {
 public:
     template<class It>
-    async_logger(const std::string& name, const It& begin, const It& end, size_t queue_size, const  async_overflow_policy overflow_policy =  async_overflow_policy::block_retry, const std::function<void()>& worker_warmup_cb = nullptr);
-    async_logger(const std::string& logger_name, sinks_init_list sinks, size_t queue_size, const  async_overflow_policy overflow_policy =  async_overflow_policy::block_retry, const std::function<void()>& worker_warmup_cb = nullptr);
-    async_logger(const std::string& logger_name, sink_ptr single_sink, size_t queue_size, const  async_overflow_policy overflow_policy =  async_overflow_policy::block_retry, const std::function<void()>& worker_warmup_cb = nullptr);
+    async_logger(const std::string& name, 
+    	const It& begin,
+    	const It& end,
+    	size_t queue_size,
+    	const async_overflow_policy overflow_policy =  async_overflow_policy::block_retry,
+    	const std::function<void()>& worker_warmup_cb = nullptr);
+    	
+    async_logger(const std::string& logger_name,
+    	sinks_init_list sinks,
+    	size_t queue_size,
+    	const async_overflow_policy overflow_policy = async_overflow_policy::block_retry,
+    	const std::function<void()>& worker_warmup_cb = nullptr);
+    	
+    async_logger(const std::string& logger_name,
+    	sink_ptr single_sink,
+    	size_t queue_size,
+    	const async_overflow_policy overflow_policy =  async_overflow_policy::block_retry,
+    	const std::function<void()>& worker_warmup_cb = nullptr);
 
 
 protected:
