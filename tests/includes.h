2f81e54568af44ad980808321b152e6679df1fef
gabime <gmelman1@gmail.com>
2015-05-15 20:30:37 +0300

    Added unit tests (catch based)

---
+++ tests/includes.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdio>
+#include <fstream>
+#include <string>
+#include <ostream>
+#include <chrono>
+#include "catch.hpp"
+#include "../include/spdlog/spdlog.h"
+#include "../include/spdlog/sinks/null_sink.h"
