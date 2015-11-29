8c55ee1a557477512fe264fd34cadf56dab2d647
Gabi Melman <gmelman1@gmail.com>
2015-11-29 15:56:42 +0200

    Update async_log_helper.h
    Fixed shadow warning in gcc

--- include/spdlog/details/async_log_helper.h
+++ include/spdlog/details/async_log_helper.h
@@ -61,7 +61,7 @@ async_msg(async_msg&& other) SPDLOG_NOEXCEPT:
                     msg_type(std::move(other.msg_type))
         {}
 
-        async_msg(async_msg_type msg_type) :msg_type(msg_type)
+        async_msg(async_msg_type m_type) :msg_type(m_type)
         {};
 
         async_msg& operator=(async_msg&& other) SPDLOG_NOEXCEPT
