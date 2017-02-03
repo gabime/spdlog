054eb555ca0a37b5b75c36618dfc0d1be6295316
Gabi Melman <gmelman1@gmail.com>
2017-02-03 15:28:32 +0200

    Moved `SPDLOG_VERSION` macro to spdlog.h

--- include/spdlog/spdlog.h
+++ include/spdlog/spdlog.h
@@ -2,7 +2,6 @@
 // Copyright(c) 2015 Gabi Melman.
 // Distributed under the MIT License (http://opensource.org/licenses/MIT)
 //
-
 // spdlog main header file.
 // see example.cpp for usage example
 
