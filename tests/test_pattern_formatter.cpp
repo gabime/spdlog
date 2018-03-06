e8a39c894ffbb3e03328278e4be7a106a31a433e
Gabi Melman <gmelman1@gmail.com>
2018-03-06 10:31:36 +0200

    Update test_pattern_formatter.cpp

--- tests/test_pattern_formatter.cpp
+++ tests/test_pattern_formatter.cpp
@@ -56,6 +56,6 @@ TEST_CASE("date MM/DD/YY ", "[pattern_formatter]")
     auto formatter = std::make_shared<spdlog::pattern_formatter>("%D %v", spdlog::pattern_time_type::local, "\n");
     auto now_tm = spdlog::details::os::localtime();
     std::stringstream oss;
-    oss << std::setfill('0') << std::setw(2) << now_tm.tm_mon + 1 << "/" << now_tm.tm_mday << "/" << (now_tm.tm_year + 1900) % 1000 << " Some message\n";
+    oss << std::setfill('0') << std::setw(2) << now_tm.tm_mon + 1 << "/" << std::setw(2) << now_tm.tm_mday << "/" << std::setw(2) << (now_tm.tm_year + 1900) % 1000 << " Some message\n";
     REQUIRE(log_to_str("Some message", formatter) == oss.str());
 }
