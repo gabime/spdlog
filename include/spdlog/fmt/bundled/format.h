cbe98c0fd236078eedc51f72d93a6fee4d2e2148
gabime <gmelman1@gmail.com>
2018-03-09 15:30:48 +0200

    clang format

--- include/spdlog/fmt/bundled/format.h
+++ include/spdlog/fmt/bundled/format.h
@@ -2898,8 +2898,9 @@ inline uint64_t make_type(FMT_GEN15(FMT_ARG_TYPE_DEFAULT))
     FMT_WRAP1(func, arg_type, 1)                                                                                                           \
     FMT_WRAP1(func, arg_type, 2)                                                                                                           \
     FMT_WRAP1(func, arg_type, 3)                                                                                                           \
-    FMT_WRAP1(func, arg_type, 4) FMT_WRAP1(func, arg_type, 5) FMT_WRAP1(func, arg_type, 6) FMT_WRAP1(func, arg_type, 7)                    \
-        FMT_WRAP1(func, arg_type, 8) FMT_WRAP1(func, arg_type, 9) FMT_WRAP1(func, arg_type, 10)
+    FMT_WRAP1(func, arg_type, 4)                                                                                                           \
+    FMT_WRAP1(func, arg_type, 5) FMT_WRAP1(func, arg_type, 6) FMT_WRAP1(func, arg_type, 7) FMT_WRAP1(func, arg_type, 8)                    \
+        FMT_WRAP1(func, arg_type, 9) FMT_WRAP1(func, arg_type, 10)
 
 #define FMT_CTOR(ctor, func, arg0_type, arg1_type, n)                                                                                      \
     template <FMT_GEN(n, FMT_MAKE_TEMPLATE_ARG)> ctor(arg0_type arg0, arg1_type arg1, FMT_GEN(n, FMT_MAKE_ARG))                            \
