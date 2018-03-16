1108515738aa28ca619a32e0fc17f44dc011cbb9
gabime <gmelman1@gmail.com>
2018-03-16 17:04:35 +0200

    format.sh small fix

--- format.sh
+++ format.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 find . -name "*\.h" -o -name "*\.cpp"|xargs dos2unix
-find . -name "*\.h" -o -name "*\.cpp"|xargs clang-format-5.0 -i
+find . -name "*\.h" -o -name "*\.cpp"|xargs clang-format -i
 
 
