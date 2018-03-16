ea95ea8295ecfe43011ff1997cfd6024996c5418
gabime <gmelman1@gmail.com>
2018-03-16 17:20:56 +0200

    Fix potential issue #660

--- include/spdlog/fmt/ostr.h
+++ include/spdlog/fmt/ostr.h
@@ -4,10 +4,13 @@
 //
 
 #pragma once
-
-// include external or bundled copy of fmtlib's ostream support
+//
+// include bundled or external copy of fmtlib's ostream support
 //
 #if !defined(SPDLOG_FMT_EXTERNAL)
+#ifndef FMT_HEADER_ONLY
+#define FMT_HEADER_ONLY
+#endif
 #include "bundled/ostream.h"
 #include "fmt.h"
 #else
