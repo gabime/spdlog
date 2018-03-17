c739e68021b4fdeaf22b3612fc1ed1dfe34021d1
gabime <gmelman1@gmail.com>
2018-03-17 12:47:04 +0200

    clang format namespace fix

--- format.sh
+++ format.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-find . -name "*\.h" -o -name "*\.cpp"|xargs dos2unix
-find . -name "*\.h" -o -name "*\.cpp"|xargs clang-format -i
+echo -n "Running dos2unix "
+find . -name "*\.h" -o -name "*\.cpp"|xargs -I {} sh -c "dos2unix '{}' 2>/dev/null; echo -n '.'"
+echo
+echo -n "Running clang-format "
+find . -name "*\.h" -o -name "*\.cpp"|xargs -I {} sh -c "clang-format -i {}; echo -n '.'"
+echo
 
 
