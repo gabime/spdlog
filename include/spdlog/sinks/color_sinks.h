b9fac2b179a46436762296506d63a2b365691f46
gabime <gmelman1@gmail.com>
2018-04-19 18:40:58 +0300

    added missing header

---
+++ include/spdlog/sinks/color_sinks.h
@@ -0,0 +1,12 @@
+//
+// Copyright(c) 2015 Gabi Melman.
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+//
+#pragma once
+
+#ifdef _WIN32
+#include "wincolor_sink.h"
+#else
+#include "ansicolor_sink.h"
+#endif
+
