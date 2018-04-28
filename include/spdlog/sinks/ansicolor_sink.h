9b5c5b4f3d4beaba621fd623149376e1e3859db6
gabime <gmelman1@gmail.com>
2018-04-29 00:20:04 +0300

    Small template rename

--- include/spdlog/sinks/ansicolor_sink.h
+++ include/spdlog/sinks/ansicolor_sink.h
@@ -22,13 +22,13 @@ namespace sinks {
  * of the message.
  * If no color terminal detected, omit the escape codes.
  */
-template<class StdoutTrait, class ConsoleMutexTrait>
+template<class ConsoleStdoutTrait, class ConsoleMutexTrait>
 class ansicolor_sink : public sink
 {
 public:
     using mutex_t = typename ConsoleMutexTrait::mutex_t;
     ansicolor_sink()
-        : target_file_(StdoutTrait::stream())
+        : target_file_(ConsoleStdoutTrait::stream())
         , _mutex(ConsoleMutexTrait::console_mutex())
 
     {
