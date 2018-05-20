8bd16feb3604267d07d6275af21e3cc749e29c05
gabime <gmelman1@gmail.com>
2018-05-20 18:01:47 +0300

    format

--- example/example.cpp
+++ example/example.cpp
@@ -105,7 +105,7 @@ int main(int, char *[])
 
 void async_example()
 {
-    size_t q_size = 4096; 
+    size_t q_size = 4096;
     spdlog::set_async_mode(q_size);
     auto async_file = spd::daily_logger_st("async_file_logger", "logs/async_log.txt");
     for (int i = 0; i < 100; ++i)
