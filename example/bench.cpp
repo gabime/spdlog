c3dd37c8c1b1868a3c8ffb759bc3af62ef1e2cfc
gabime <gmelman1@gmail.com>
2018-05-20 17:41:22 +0300

    updated bench to show elapsed too

--- example/bench.cpp
+++ example/bench.cpp
@@ -100,7 +100,7 @@ void bench(int howmany, std::shared_ptr<spdlog::logger> log)
 
     auto delta = system_clock::now() - start;
     auto delta_d = duration_cast<duration<double>>(delta).count();
-    cout << format(int(howmany / delta_d)) << "/sec" << endl;
+    cout << "Elapsed: " << delta_d << "\t" << format(int(howmany / delta_d)) << "/sec" << endl;
 }
 
 void bench_mt(int howmany, std::shared_ptr<spdlog::logger> log, int thread_count)
@@ -130,5 +130,5 @@ void bench_mt(int howmany, std::shared_ptr<spdlog::logger> log, int thread_count
 
     auto delta = system_clock::now() - start;
     auto delta_d = duration_cast<duration<double>>(delta).count();
-    cout << format(int(howmany / delta_d)) << "/sec" << endl;
+    cout << "Elapsed: " << delta_d << "\t" << format(int(howmany / delta_d)) << "/sec" << endl;
 }
