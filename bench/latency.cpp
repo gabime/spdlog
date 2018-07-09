60ca07c2d529a92e1290c23e65355c2655730d0d
gabime <gmelman1@gmail.com>
2018-07-09 23:56:07 +0300

    fixed latency test

--- bench/latency.cpp
+++ bench/latency.cpp
@@ -32,7 +32,7 @@ void bench_mt(int howmany, std::shared_ptr<spdlog::logger> log, int thread_count
 int main(int , char *[])
 {
     std::srand(std::time(nullptr)); // use current time as seed for random generator
-    int howmany = 10000;
+    int howmany = 1000000;
     int queue_size = howmany + 2;
     int threads = 10;
     int file_size = 30 * 1024 * 1024;
