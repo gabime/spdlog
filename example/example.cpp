97c201297f8599ea6399c5c3ca3fe5ba1c6c4388
gabime <gmelman1@gmail.com>
2018-07-10 10:40:25 +0300

    Udpated example

--- example/example.cpp
+++ example/example.cpp
@@ -171,7 +171,7 @@ void user_defined_example()
 void err_handler_example()
 {
     // can be set globally or per logger(logger->set_error_handler(..))
-    spdlog::set_error_handler([](const std::string &msg) { spdlog::get("console")->error("*** LOGGER ERROR ***: {}", msg); });
+    spdlog::set_error_handler([](const std::string &msg) { spdlog::get("console")->error("*** ERROR HANDLER EXAMPLE ***: {}", msg); });
     spdlog::get("console")->info("some invalid message to trigger an error {}{}{}{}", 3);
 }
 
