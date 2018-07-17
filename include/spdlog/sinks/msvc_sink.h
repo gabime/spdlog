b238bf95127e9f70cc2bdadac811a0cbfa163c3c
Gabi Melman <gmelman1@gmail.com>
2018-07-17 20:09:05 +0300

    fix issue #755

--- include/spdlog/sinks/msvc_sink.h
+++ include/spdlog/sinks/msvc_sink.h
@@ -27,8 +27,11 @@ public:
     explicit msvc_sink() {}
 
 protected:
-    void sink_it_(const details::log_msg &, const fmt::memory_buffer &formatted) override
+    void sink_it_(const details::log_msg &msg) override
     {
+        
+        fmt::memory_buffer formatted;
+        sink::formatter_->format(msg, formatted);
         OutputDebugStringA(fmt::to_string(formatted).c_str());
     }
 
