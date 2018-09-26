a8c4aef6bd97aa37292819baf443862c263be2ae
gabime <gmelman1@gmail.com>
2018-09-26 23:50:47 +0300

    fix typo

--- include/spdlog/common.h
+++ include/spdlog/common.h
@@ -178,7 +178,7 @@ using std::make_unique;
 template<typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args &&... args)
 {
-    static_assert(!std::is_array<T>::value, "arrays to not supported");
+    static_assert(!std::is_array<T>::value, "arrays not supported");
     return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 #endif
