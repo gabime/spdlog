5cd24f30338c10539c6434a742ff0e8ad5e4ff91
gabime <gmelman1@gmail.com>
2018-09-26 15:26:27 +0300

    Replaced SPDLOG_FINAL macro with final

--- include/spdlog/sinks/wincolor_sink.h
+++ include/spdlog/sinks/wincolor_sink.h
@@ -61,7 +61,7 @@ public:
         colors_[level] = color;
     }
 
-    void log(const details::log_msg &msg) SPDLOG_FINAL override
+    void log(const details::log_msg &msg) final override
     {
         std::lock_guard<mutex_t> lock(mutex_);
         fmt::memory_buffer formatted;
@@ -85,18 +85,18 @@ public:
         }
     }
 
-    void flush() SPDLOG_FINAL override
+    void flush() final override
     {
         // windows console always flushed?
     }
 
-    void set_pattern(const std::string &pattern) override SPDLOG_FINAL
+    void set_pattern(const std::string &pattern) override final
     {
         std::lock_guard<mutex_t> lock(mutex_);
         formatter_ = std::unique_ptr<spdlog::formatter>(new pattern_formatter(pattern));
     }
 
-    void set_formatter(std::unique_ptr<spdlog::formatter> sink_formatter) override SPDLOG_FINAL
+    void set_formatter(std::unique_ptr<spdlog::formatter> sink_formatter) override final
     {
         std::lock_guard<mutex_t> lock(mutex_);
         formatter_ = std::move(sink_formatter);
