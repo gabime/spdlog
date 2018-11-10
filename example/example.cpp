e933c5f4816cb88e13e1595324c03b53cc218e30
gabime <gmelman1@gmail.com>
2018-11-11 01:11:09 +0200

    Fixed example

--- example/example.cpp
+++ example/example.cpp
@@ -28,32 +28,9 @@ void clone_example();
 
 int main(int, char *[])
 {
-
-    spdlog::set_pattern("[%-8v]");
-    spdlog::info("LEFT");
-    spdlog::info("123");
-    spdlog::info("1234");
-    spdlog::info("12345678");
-    spdlog::info("123456789");
-
-
-    spdlog::set_pattern("[%=8v]");
-    spdlog::info("");
-    spdlog::info("CENTER");
-    spdlog::info("123");
-    spdlog::info("1234");
-    spdlog::info("12345678");
-    spdlog::info("123456789");
-
-    spdlog::set_pattern("[%8v]");
-    spdlog::info("");
-    spdlog::info("RIGHT");
-    spdlog::info("123");
-    spdlog::info("1234");
-    spdlog::info("12345678");
-    spdlog::info("123456789");
-
-    return 0;
+    spdlog::info("Welcome to spdlog version {}.{}.{} !", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR, SPDLOG_VER_PATCH);
+    spdlog::warn("Easy padding in numbers like {:08d}", 12);
+    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
     spdlog::info("Support for floats {:03.2f}", 1.23456);
     spdlog::info("Positional args are {1} {0}..", "too", "supported");
     spdlog::info("{:>8} aligned, {:>8} aligned", "right", "left");
