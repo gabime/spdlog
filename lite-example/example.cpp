c29b7d22d96999dcb696a16a5d1dfc804defd10a
gabime <gmelman1@gmail.com>
2019-03-23 16:39:05 +0200

    wip lite

--- lite-example/example.cpp
+++ lite-example/example.cpp
@@ -1,7 +1,11 @@
+#define SPDLITE_ACTIVE_LEVEL SPDLITE_LEVEL_INFO
 #include "logger.h"
 
 int main()
 {
     //auto l = spdlog::create_lite();
-    spdlog::lite::info("HELLO info {}", 123);
+    //spdlog::lite::info("HELLO info {}", 123);
+    SPDLITE_TRACE("SOME MACRO {}", 123);
+    SPDLITE_INFO("SOME MACRO {}", "HHHHH");
+
 }
