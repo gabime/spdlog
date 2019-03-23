221ce33eb5e37411e67c61494e9728677cc93605
gabime <gmelman1@gmail.com>
2019-03-24 01:23:14 +0200

    renamed printf

--- lite-example/example.cpp
+++ lite-example/example.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-    auto l = spdlog::create_lite((void*)"async");
+    auto l = spdlog::create_lite();
     l.set_level(spdlog::lite::level::trace);
 
     l.trace_printf("Hello %s ", "GABI");
     l.info_printf("Hello %d", 12346);
     l.warn_printf("Hello %f", 12346.5656);
-    l.warn("Hello {}", 12346.5656);
+    l.warn("Hello {}", "LITE :) ");
 }
