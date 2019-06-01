52b6be0dfe1f2a4c7155a7804e27bd2da4357e39
gabime <gmelman1@gmail.com>
2019-06-01 14:57:57 +0300

    Added logger ctor that accepts only name(empty logger)

--- include/spdlog/logger.h
+++ include/spdlog/logger.h
@@ -31,18 +31,29 @@ namespace spdlog {
 class logger
 {
 public:
+    // Empty logger
+    logger(std::string name) :
+            name_(std::move(name)),
+            sinks_(){}
+
+    // Logger with range on sinks
     template<typename It>
     logger(std::string name, It begin, It end)
         : name_(std::move(name))
         , sinks_(begin, end)
     {}
 
+    // Logger with single sink
     logger(std::string name, sink_ptr single_sink)
         : logger(std::move(name), {std::move(single_sink)})
     {}
+
+    // Logger with sinks init list
     logger(std::string name, sinks_init_list sinks)
         : logger(std::move(name), sinks.begin(), sinks.end())
     {}
+
+
 
     virtual ~logger() = default;
 
