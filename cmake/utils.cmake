8d3d06b7a0c645a2dae8f2b299781aec10b5d6ad
gabime <gmelman1@gmail.com>
2019-06-10 19:53:39 +0300

    Fixed version extraction in CMake

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -2,16 +2,23 @@
 function(spdlog_extract_version)
 	file(READ "${CMAKE_CURRENT_LIST_DIR}/include/spdlog/version.h" file_contents)
 	string(REGEX MATCH "SPDLOG_VER_MAJOR ([0-9]+)" _  "${file_contents}")
+	if(NOT CMAKE_MATCH_COUNT EQUAL 1)
+        message(FATAL_ERROR "Could not extract major version number from spdlog/version.h")
+	endif()
 	set(ver_major ${CMAKE_MATCH_1})
 
 	string(REGEX MATCH "SPDLOG_VER_MINOR ([0-9]+)" _  "${file_contents}")
+	if(NOT CMAKE_MATCH_COUNT EQUAL 1)
+        message(FATAL_ERROR "Could not extract minor version number from spdlog/version.h")
+	endif()
+
 	set(ver_minor ${CMAKE_MATCH_1})
 	string(REGEX MATCH "SPDLOG_VER_PATCH ([0-9]+)" _  "${file_contents}")
+	if(NOT CMAKE_MATCH_COUNT EQUAL 1)
+        message(FATAL_ERROR "Could not extract patch version number from spdlog/version.h")
+	endif()
 	set(ver_patch ${CMAKE_MATCH_1})
 
-	if (NOT ver_major OR NOT ver_minor OR NOT ver_patch)
-		message(FATAL_ERROR "Could not extract valid version from spdlog/version.h")
-	endif()
 	set (SPDLOG_VERSION "${ver_major}.${ver_minor}.${ver_patch}" PARENT_SCOPE)
 endfunction()
 
