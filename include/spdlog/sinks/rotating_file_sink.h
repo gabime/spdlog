67f3a83c314931beeb34b1ddad58f2ef90a6ba04
casperevan <49881003+casperevan@users.noreply.github.com>
2019-06-25 13:04:48 -0700

    Fix missing include for null_mutex in rotating_file_sink.h

--- include/spdlog/sinks/rotating_file_sink.h
+++ include/spdlog/sinks/rotating_file_sink.h
@@ -5,6 +5,7 @@
 
 #include "spdlog/sinks/base_sink.h"
 #include "spdlog/details/file_helper.h"
+#include "spdlog/details/null_mutex.h"
 #include "spdlog/details/synchronous_factory.h"
 
 #include <chrono>
