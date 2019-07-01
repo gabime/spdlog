2a2a34601c316f540b1dd21d7ca36f613e10c783
Daniel Mensinger <daniel@mensinger-ka.de>
2019-07-01 12:37:18 +0200

    moved scripts into subdirectory

--- scripts/extract_version.py
+++ scripts/extract_version.py
@@ -3,7 +3,7 @@
 import os
 import re
 
-base_path = os.path.abspath(os.path.dirname(__file__))
+base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 config_h  = os.path.join(base_path, 'include', 'spdlog', 'version.h')
 data      = {'MAJOR': 0, 'MINOR': 0, 'PATCH': 0}
 reg       = re.compile(r'^\s*#define\s+SPDLOG_VER_([A-Z]+)\s+([0-9]+).*$')
