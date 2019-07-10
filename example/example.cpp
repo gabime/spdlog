84809db95557a8f7bc8be611556159187bfdf7b6
Gabi Melman <gmelman1@gmail.com>
2019-07-10 03:07:17 +0300

    Update example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -23,7 +23,6 @@ void clone_example();
 
 int main(int, char *[])
 {
-    syslog_example();
     spdlog::info("Welcome to spdlog version {}.{}.{}  !", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR, SPDLOG_VER_PATCH);
     spdlog::warn("Easy padding in numbers like {:08d}", 12);
     spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
