789fb1e7c9d850f237aae805d70168d090932dd5
k0zmo <k0zmo@outlook.com>
2019-07-13 10:54:22 +0200

    Move templated constructor definition to header file

--- include/spdlog/async_logger-inl.h
+++ include/spdlog/async_logger-inl.h
@@ -14,14 +14,6 @@
 #include <memory>
 #include <string>
 
-template<typename It>
-SPDLOG_INLINE spdlog::async_logger::async_logger(
-    std::string logger_name, It begin, It end, std::weak_ptr<details::thread_pool> tp, async_overflow_policy overflow_policy)
-    : logger(std::move(logger_name), begin, end)
-    , thread_pool_(std::move(tp))
-    , overflow_policy_(overflow_policy)
-{}
-
 SPDLOG_INLINE spdlog::async_logger::async_logger(
     std::string logger_name, sinks_init_list sinks_list, std::weak_ptr<details::thread_pool> tp, async_overflow_policy overflow_policy)
     : async_logger(std::move(logger_name), sinks_list.begin(), sinks_list.end(), std::move(tp), overflow_policy)
