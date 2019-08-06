dcd590b9deacab89c0e0fd1f18e20967cf4c4ce7
gabime <gmelman1@gmail.com>
2019-08-07 00:59:29 +0300

    fstat64(..) -> ::fstat64(..)

--- include/spdlog/details/os-inl.h
+++ include/spdlog/details/os-inl.h
@@ -216,7 +216,7 @@ SPDLOG_INLINE size_t filesize(FILE *f)
 // 64 bits(but not in osx or cygwin, where fstat64 is deprecated)
 #if !defined(__FreeBSD__) && !defined(__APPLE__) && (defined(__x86_64__) || defined(__ppc64__)) && !defined(__CYGWIN__)
     struct stat64 st;
-    if (fstat64(fd, &st) == 0)
+    if (::fstat64(fd, &st) == 0)
     {
         return static_cast<size_t>(st.st_size);
     }
