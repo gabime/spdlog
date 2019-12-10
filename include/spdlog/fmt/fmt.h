ab178057db46885a0778d6c1cab7cdbcd8a712e1
gabime <gmelman1@gmail.com>
2019-12-10 03:00:06 +0200

    Fixed (maybe) #1348

--- include/spdlog/fmt/fmt.h
+++ include/spdlog/fmt/fmt.h
@@ -26,8 +26,9 @@
 #ifndef FMT_USE_WINDOWS_H
 #define FMT_USE_WINDOWS_H 0
 #endif
-#include "bundled/core.h"
-#include "bundled/format.h"
+
+#include <spdlog/fmt/bundled/core.h>
+#include <spdlog/fmt/bundled/format.h>
 #else // SPDLOG_FMT_EXTERNAL is defined - use external fmtlib
 #include <fmt/core.h>
 #include <fmt/format.h>
--- include/spdlog/fmt/ostr.h
+++ include/spdlog/fmt/ostr.h
@@ -7,12 +7,14 @@
 //
 // include bundled or external copy of fmtlib's ostream support
 //
+
 #if !defined(SPDLOG_FMT_EXTERNAL)
+#ifdef SPDLOG_HEADER_ONLY
 #ifndef FMT_HEADER_ONLY
 #define FMT_HEADER_ONLY
 #endif
-#include "bundled/ostream.h"
-#include "fmt.h"
+#endif
+#include <spdlog/fmt/bundled/ostream.h>
 #else
 #include <fmt/ostream.h>
 #endif
