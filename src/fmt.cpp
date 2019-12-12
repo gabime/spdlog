d52e825bbc7c25566f822d9aef6ff2ba3a3d603b
gabime <gmelman1@gmail.com>
2019-12-13 00:12:05 +0200

    clang-format

--- src/fmt.cpp
+++ src/fmt.cpp
@@ -13,12 +13,10 @@ FMT_BEGIN_NAMESPACE
 template struct FMT_API internal::basic_data<void>;
 
 // Workaround a bug in MSVC2013 that prevents instantiation of format_float.
-int (*instantiate_format_float)(double, int, internal::float_specs,
-                                internal::buffer<char>&) =
-internal::format_float;
+int (*instantiate_format_float)(double, int, internal::float_specs, internal::buffer<char> &) = internal::format_float;
 
 #ifndef FMT_STATIC_THOUSANDS_SEPARATOR
-template FMT_API internal::locale_ref::locale_ref(const std::locale& loc);
+template FMT_API internal::locale_ref::locale_ref(const std::locale &loc);
 template FMT_API std::locale internal::locale_ref::get<std::locale>() const;
 #endif
 
@@ -28,28 +26,18 @@ template FMT_API std::string internal::grouping_impl<char>(locale_ref);
 template FMT_API char internal::thousands_sep_impl(locale_ref);
 template FMT_API char internal::decimal_point_impl(locale_ref);
 
-template FMT_API void internal::buffer<char>::append(const char*, const char*);
+template FMT_API void internal::buffer<char>::append(const char *, const char *);
 
-template FMT_API void internal::arg_map<format_context>::init(
-        const basic_format_args<format_context>& args);
+template FMT_API void internal::arg_map<format_context>::init(const basic_format_args<format_context> &args);
 
-template FMT_API std::string internal::vformat<char>(
-        string_view, basic_format_args<format_context>);
+template FMT_API std::string internal::vformat<char>(string_view, basic_format_args<format_context>);
 
-template FMT_API format_context::iterator internal::vformat_to(
-        internal::buffer<char>&, string_view, basic_format_args<format_context>);
+template FMT_API format_context::iterator internal::vformat_to(internal::buffer<char> &, string_view, basic_format_args<format_context>);
 
-template FMT_API int internal::snprintf_float(double, int,
-                                              internal::float_specs,
-                                              internal::buffer<char>&);
-template FMT_API int internal::snprintf_float(long double, int,
-                                              internal::float_specs,
-                                              internal::buffer<char>&);
-template FMT_API int internal::format_float(double, int, internal::float_specs,
-                                            internal::buffer<char>&);
-template FMT_API int internal::format_float(long double, int,
-                                            internal::float_specs,
-                                            internal::buffer<char>&);
+template FMT_API int internal::snprintf_float(double, int, internal::float_specs, internal::buffer<char> &);
+template FMT_API int internal::snprintf_float(long double, int, internal::float_specs, internal::buffer<char> &);
+template FMT_API int internal::format_float(double, int, internal::float_specs, internal::buffer<char> &);
+template FMT_API int internal::format_float(long double, int, internal::float_specs, internal::buffer<char> &);
 
 // Explicit instantiations for wchar_t.
 
@@ -57,11 +45,9 @@ template FMT_API std::string internal::grouping_impl<wchar_t>(locale_ref);
 template FMT_API wchar_t internal::thousands_sep_impl(locale_ref);
 template FMT_API wchar_t internal::decimal_point_impl(locale_ref);
 
-template FMT_API void internal::buffer<wchar_t>::append(const wchar_t*,
-                                                        const wchar_t*);
+template FMT_API void internal::buffer<wchar_t>::append(const wchar_t *, const wchar_t *);
 
-template FMT_API std::wstring internal::vformat<wchar_t>(
-        wstring_view, basic_format_args<wformat_context>);
+template FMT_API std::wstring internal::vformat<wchar_t>(wstring_view, basic_format_args<wformat_context>);
 FMT_END_NAMESPACE
 
 #endif
