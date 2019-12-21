010b0e1d75c867bac2a978cef5b5d9c7547d996c
gabime <gmelman1@gmail.com>
2019-12-21 17:58:31 +0200

    wip

--- include/spdlog/cfg/helpers.h
+++ include/spdlog/cfg/helpers.h
@@ -4,15 +4,22 @@
 #pragma once
 
 #include <spdlog/cfg/log_levels.h>
-#include <string>
 
-//
-// Init levels from given string
-//
+
+
 
 namespace spdlog {
 namespace cfg {
 namespace helpers {
+//
+// Init levels from given string
+//
+// Examples:
+//
+// set global level to debug: "debug"
+// turn off all logging except for logger1: "off,logger1=debug"
+// turn off all logging except for logger1 and logger2: "off,logger1=debug,logger2=info"
+//
 log_levels extract_levels(const std::string &txt);
 }
 
