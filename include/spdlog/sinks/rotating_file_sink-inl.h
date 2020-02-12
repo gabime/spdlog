53a56b82af42e71c4232c80de8a42cab1709a854
Gabi Melman <gmelman1@gmail.com>
2020-02-12 07:51:42 +0200

    Update rotating_file_sink-inl.h

--- include/spdlog/sinks/rotating_file_sink-inl.h
+++ include/spdlog/sinks/rotating_file_sink-inl.h
@@ -100,7 +100,7 @@ SPDLOG_INLINE void rotating_file_sink<Mutex>::rotate_()
         }
         filename_t target = calc_filename(base_filename_, i);
 
-        if (!rename_file(src, target))
+        if (!rename_file_(src, target))
         {
             // if failed try again after a small delay.
             // this is a workaround to a windows issue, where very high rotation
@@ -121,7 +121,7 @@ SPDLOG_INLINE void rotating_file_sink<Mutex>::rotate_()
 // delete the target if exists, and rename the src file  to target
 // return true on success, false otherwise.
 template<typename Mutex>
-SPDLOG_INLINE bool rotating_file_sink<Mutex>::rename_file_(const filename_t &src_filename, const filename_t &target_filename)
+SPDLOG_INLINE bool rotating_file_sink<Mutex>::rename_file_(yconst filename_t &src_filename, const filename_t &target_filename)
 {
     // try to delete the target file in case it already exists.
     (void)details::os::remove(target_filename);
