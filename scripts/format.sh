daaa0253567c261d8e8c94f8958a1d3739e1f15d
gabime <gmelman1@gmail.com>
2020-02-25 18:52:35 +0200

    moved .clang-format to top level

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"
-
+cd "$(dirname "$0")"/..
+pwd
 echo -n "Running dos2unix     "
-find .. -name "*\.h" -o -name "*\.cpp"|grep -v bundled|xargs -I {} sh -c "dos2unix '{}' 2>/dev/null; echo -n '.'"
+find . -name "*\.h" -o -name "*\.cpp"|grep -v bundled|xargs -I {} sh -c "dos2unix '{}' 2>/dev/null; echo -n '.'"
 echo
 echo -n "Running clang-format "
-find .. -name "*\.h" -o -name "*\.cpp"|grep -v bundled|xargs -I {} sh -c "clang-format -i {}; echo -n '.'"
+find . -name "*\.h" -o -name "*\.cpp"|grep -v bundled|xargs -I {} sh -c "clang-format -i {}; echo -n '.'"
 echo
 
 
