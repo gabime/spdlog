cf6bb88af2322ed533e58c53ddbd72c502481b51
Gabi Melman <gmelman1@gmail.com>
2020-03-13 01:12:05 +0200

    Update tcp_client-windows.h

--- include/spdlog/details/tcp_client-windows.h
+++ include/spdlog/details/tcp_client-windows.h
@@ -15,7 +15,6 @@
 #include <stdio.h>
 #include <string>
 
-// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
 #pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "Mswsock.lib")
 #pragma comment(lib, "AdvApi32.lib")
