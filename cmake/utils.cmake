132ec0a5fca4503ba08d98dce54bfb489318c2b5
FIlip Demski <glamhoth@protonmail.com>
2020-04-03 10:09:29 +0200

    Added an option to disable compiler warnings

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -28,17 +28,19 @@ endfunction()
 
 # Turn on warnings on the given target
 function(spdlog_enable_warnings target_name)
-	if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-		list(APPEND MSVC_OPTIONS "/W3")
-		if(MSVC_VERSION GREATER  1900)  #Allow non fatal security wanrnings for msvc 2015
-			list(APPEND MSVC_OPTIONS "/WX")
-		endif()
-	endif()
+    if(SPDLOG_ENABLE_WARNINGS)
+        if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+            list(APPEND MSVC_OPTIONS "/W3")
+            if(MSVC_VERSION GREATER  1900)  #Allow non fatal security wanrnings for msvc 2015
+                list(APPEND MSVC_OPTIONS "/WX")
+            endif()
+        endif()
 
-    target_compile_options(${target_name} PRIVATE
-        $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
-            -Wall -Wextra -Wconversion -pedantic -Wfatal-errors>
-        $<$<CXX_COMPILER_ID:MSVC>:${MSVC_OPTIONS}>)
+        target_compile_options(${target_name} PRIVATE
+            $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
+                -Wall -Wextra -Wconversion -pedantic -Wfatal-errors>
+            $<$<CXX_COMPILER_ID:MSVC>:${MSVC_OPTIONS}>)
+    endif()
 
 endfunction()
 
