ffb1b0394362931a5815401b18659e7a795067e9
Paul Kunysch <paul@kunysch.de>
2021-04-27 14:33:55 +0200

    Check for errors in file_helper::flush.

--- include/spdlog/details/file_helper-inl.h
+++ include/spdlog/details/file_helper-inl.h
@@ -72,7 +72,10 @@ SPDLOG_INLINE void file_helper::reopen(bool truncate)
 
 SPDLOG_INLINE void file_helper::flush()
 {
-    std::fflush(fd_);
+    if (std::fflush(fd_))
+    {
+        throw_spdlog_ex("Failed flushing file " + os::filename_to_str(filename_), errno);
+    }
 }
 
 SPDLOG_INLINE void file_helper::close()
