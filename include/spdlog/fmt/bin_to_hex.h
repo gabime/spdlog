5887744d8b6ddaa3044d1c59f06e2cf24b8e0349
gabime <gmelman1@gmail.com>
2021-06-24 15:58:25 +0300

    Fixed bin2hex to work with fmt v8

--- include/spdlog/fmt/bin_to_hex.h
+++ include/spdlog/fmt/bin_to_hex.h
@@ -209,7 +209,7 @@ struct formatter<spdlog::details::dump_info<T>>
 
         if (put_positions)
         {
-            fmt::format_to(inserter, "{:<04X}: ", pos);
+            fmt::format_to(inserter, "{:04X}: ", pos);
         }
     }
 };
