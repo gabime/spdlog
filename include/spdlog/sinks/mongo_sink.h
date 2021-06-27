14950926ed7f01f5808a6d960b3ebb09c5268a80
Muhammed Galib Uludag <m231@tnctr.com>
2021-06-27 22:50:31 +0300

    ignore pattern formatting for message section in mongodb

--- include/spdlog/sinks/mongo_sink.h
+++ include/spdlog/sinks/mongo_sink.h
@@ -12,7 +12,6 @@
 
 #include "spdlog/common.h"
 #include "spdlog/details/log_msg.h"
-#include "spdlog/pattern_formatter.h"
 #include "spdlog/sinks/base_sink.h"
 #include <spdlog/details/synchronous_factory.h>
 
@@ -34,7 +33,6 @@ public:
       client_ = std::make_unique<mongocxx::client>(mongocxx::uri{uri});
       db_name_ = db_name;
       coll_name_ = collection_name;
-      set_pattern("%v");
     } catch (const std::exception) {
       throw spdlog_ex("Error opening database");
     }
@@ -48,12 +46,10 @@ protected:
     using bsoncxx::builder::stream::finalize;
 
     if (client_ != nullptr) {
-      memory_buf_t formatted;
-      base_sink<Mutex>::formatter_->format(msg, formatted);
       auto doc = document{}
                  << "timestamp" << bsoncxx::types::b_date(msg.time) << "level"
                  << level::to_string_view(msg.level).data() << "message"
-                 << std::string(formatted.begin(), formatted.end())
+                 << std::string(msg.payload.begin(), msg.payload.end())
                  << "logger_name"
                  << std::string(msg.logger_name.begin(), msg.logger_name.end())
                  << "thread_id" << static_cast<int>(msg.thread_id) << finalize;
