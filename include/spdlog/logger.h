5ba95f6816bff54b67dde2e39e0072d4fbb4590e
Gabi Melman <gmelman1@gmail.com>
2021-07-19 03:46:01 +0300

    Update logger.h

--- include/spdlog/logger.h
+++ include/spdlog/logger.h
@@ -329,7 +329,7 @@ protected:
         {
             memory_buf_t buf;
             // little bit faster than fmt::format_to(std::back_inserter(buf), fmt, std::forward<Args>(args)...);
-            fmt::detail::vformat_to(buf, fmt::to_string_view(fmt), fmt::make_format_args(args...), {});
+            fmt::detail::vformat_to(buf, fmt::string_view(fmt), fmt::make_format_args(args...), {});
             details::log_msg log_msg(loc, name_, lvl, string_view_t(buf.data(), buf.size()));
             log_it_(log_msg, log_enabled, traceback_enabled);
         }
