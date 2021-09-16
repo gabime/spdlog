2160f8dfbfa563b297d27aff7a2b012931cd195f
gabime <gmelman1@gmail.com>
2021-09-17 02:53:45 +0300

    clang-format

--- tests/test_cfg.cpp
+++ tests/test_cfg.cpp
@@ -18,131 +18,116 @@ TEST_CASE("env", "[cfg]")
 #endif
     auto levels = load_env_levels();
     auto it = levels.find(spdlog::default_logger()->name());
-    REQUIRE(levels == spdlog::cfg::level_map {{"", spdlog::level::warn}});
+    REQUIRE(levels == spdlog::cfg::level_map{{"", spdlog::level::warn}});
 }
 
 TEST_CASE("argv1", "[cfg]")
 {
     const char *argv[] = {"ignore", "SPDLOG_LEVEL=l1=warn"};
-    auto levels= load_argv_levels(sizeof(argv)/sizeof (const char*), argv);
-    REQUIRE(levels == spdlog::cfg::level_map {{"l1", spdlog::level::warn}});
+    auto levels = load_argv_levels(sizeof(argv) / sizeof(const char *), argv);
+    REQUIRE(levels == spdlog::cfg::level_map{{"l1", spdlog::level::warn}});
 }
 
 TEST_CASE("argv2", "[cfg]")
 {
     const char *argv[] = {"ignore", "SPDLOG_LEVEL=l1=warn,trace"};
-    auto levels = load_argv_levels(sizeof(argv)/sizeof (const char*), argv);
-    REQUIRE(levels == spdlog::cfg::level_map {
-                          {"l1", spdlog::level::warn},
-                          {"", spdlog::level::trace}
-                      });
+    auto levels = load_argv_levels(sizeof(argv) / sizeof(const char *), argv);
+    REQUIRE(levels == spdlog::cfg::level_map{{"l1", spdlog::level::warn}, {"", spdlog::level::trace}});
 }
 
 TEST_CASE("argv3", "[cfg]")
 {
     const char *argv[] = {"ignore", "SPDLOG_LEVEL=junk_name=warn"};
-    auto levels= load_argv_levels(sizeof(argv)/sizeof (const char*), argv);
-    REQUIRE(levels == spdlog::cfg::level_map {{"junk_name", spdlog::level::warn}});
+    auto levels = load_argv_levels(sizeof(argv) / sizeof(const char *), argv);
+    REQUIRE(levels == spdlog::cfg::level_map{{"junk_name", spdlog::level::warn}});
 }
 
 TEST_CASE("argv4", "[cfg]")
 {
     const char *argv[] = {"ignore", "SPDLOG_LEVEL=junk"};
-    auto levels= load_argv_levels(sizeof(argv)/sizeof (const char*), argv);
+    auto levels = load_argv_levels(sizeof(argv) / sizeof(const char *), argv);
     REQUIRE(levels.empty());
 }
 
 TEST_CASE("argv5", "[cfg]")
 {
     const char *argv[] = {"ignore", "ignore", "SPDLOG_LEVEL=l1=warn,trace"};
-    auto levels= load_argv_levels(sizeof(argv)/sizeof (const char*), argv);
-    REQUIRE(levels == spdlog::cfg::level_map {
-                          {"l1", spdlog::level::warn},
-                          {"", spdlog::level::trace}
-                      });
+    auto levels = load_argv_levels(sizeof(argv) / sizeof(const char *), argv);
+    REQUIRE(levels == spdlog::cfg::level_map{{"l1", spdlog::level::warn}, {"", spdlog::level::trace}});
 }
 
 TEST_CASE("argv6", "[cfg]")
 {
     const char *argv[] = {"SPDLOG_LEVEL=info"};
-    auto levels= load_argv_levels(sizeof(argv)/sizeof (const char*), argv);
+    auto levels = load_argv_levels(sizeof(argv) / sizeof(const char *), argv);
     REQUIRE(levels.empty());
 }
 
 TEST_CASE("argv7", "[cfg]")
 {
     const char *argv[] = {"ignore", ""};
-    auto levels= load_argv_levels(sizeof(argv)/sizeof (const char*), argv);
+    auto levels = load_argv_levels(sizeof(argv) / sizeof(const char *), argv);
     REQUIRE(levels.empty());
 }
 
 TEST_CASE("argv8", "[cfg]")
 {
     const char *argv[] = {"ignore"};
-    auto levels= load_argv_levels(sizeof(argv)/sizeof (const char*), argv);
+    auto levels = load_argv_levels(sizeof(argv) / sizeof(const char *), argv);
     REQUIRE(levels.empty());
 }
-
 
 TEST_CASE("argv9", "[cfg]")
 {
     const char *argv[] = {"ignore", "arg1"};
-    auto levels= load_argv_levels(sizeof(argv)/sizeof (const char*), argv);
+    auto levels = load_argv_levels(sizeof(argv) / sizeof(const char *), argv);
     REQUIRE(levels.empty());
 }
 TEST_CASE("argv10", "[cfg]")
 {
     const char *argv[] = {"ignore", "junk", "SPDLOG_LEVEL=info"};
-    auto levels= load_argv_levels(sizeof(argv)/sizeof (const char*), argv);
-    REQUIRE(levels == spdlog::cfg::level_map {{"", spdlog::level::info}});
+    auto levels = load_argv_levels(sizeof(argv) / sizeof(const char *), argv);
+    REQUIRE(levels == spdlog::cfg::level_map{{"", spdlog::level::info}});
 }
 
 TEST_CASE("argv11", "[cfg]")
 {
     const char *argv[] = {};
-    auto levels= load_argv_levels(0, argv);
+    auto levels = load_argv_levels(0, argv);
     REQUIRE(levels.empty());
 }
 
 TEST_CASE("argv12", "[cfg]")
 {
     const char *argv[] = {"ignore", "junk", "SPDLOG_LEVEL=info,l1=debug,l2=warn,l3=off"};
-    auto levels= load_argv_levels(sizeof(argv)/sizeof (const char*), argv);
-    REQUIRE(levels == spdlog::cfg::level_map {
-                          {"", spdlog::level::info},
-                          {"l1", spdlog::level::debug},
-                          {"l2", spdlog::level::warn},
-                          {"l3", spdlog::level::off}
-                      });
+    auto levels = load_argv_levels(sizeof(argv) / sizeof(const char *), argv);
+    REQUIRE(levels == spdlog::cfg::level_map{{"", spdlog::level::info}, {"l1", spdlog::level::debug}, {"l2", spdlog::level::warn},
+                          {"l3", spdlog::level::off}});
 }
 
 TEST_CASE("argv13", "[cfg]")
 {
-    //test that only first  SPDLOG_LEVEL is handled
+    // test that only first  SPDLOG_LEVEL is handled
     const char *argv[] = {"ignore", "junk", "SPDLOG_LEVEL=info,l1=debug,l2=warn,l3=off", "SPDLOG_LEVEL=off"};
-    auto levels= load_argv_levels(sizeof(argv)/sizeof (const char*), argv);
-    REQUIRE(levels == spdlog::cfg::level_map {
-                          {"", spdlog::level::info},
-                          {"l1", spdlog::level::debug},
-                          {"l2", spdlog::level::warn},
-                          {"l3", spdlog::level::off}
-                      });
+    auto levels = load_argv_levels(sizeof(argv) / sizeof(const char *), argv);
+    REQUIRE(levels == spdlog::cfg::level_map{{"", spdlog::level::info}, {"l1", spdlog::level::debug}, {"l2", spdlog::level::warn},
+                          {"l3", spdlog::level::off}});
 }
 
 TEST_CASE("argv14", "[cfg]")
 {
-    //test that only first  SPDLOG_LEVEL is handled
+    // test that only first  SPDLOG_LEVEL is handled
     const char *argv[] = {"ignore", "SPDLOG_LEVEL="};
-    auto levels= load_argv_levels(sizeof(argv)/sizeof (const char*), argv);
+    auto levels = load_argv_levels(sizeof(argv) / sizeof(const char *), argv);
     REQUIRE(levels.empty());
 }
 
 TEST_CASE("argv15", "[cfg]")
 {
-    //test that only first  SPDLOG_LEVEL is handled
+    // test that only first  SPDLOG_LEVEL is handled
     const char *argv[] = {"ignore", "SPDLOG_LEVEL=info,l1=junk,l2=debug"};
-    auto levels= load_argv_levels(sizeof(argv)/sizeof (const char*), argv);
-    REQUIRE(levels == spdlog::cfg::level_map {
+    auto levels = load_argv_levels(sizeof(argv) / sizeof(const char *), argv);
+    REQUIRE(levels == spdlog::cfg::level_map{
                           {"", spdlog::level::info},
                           {"l2", spdlog::level::debug},
                       });
