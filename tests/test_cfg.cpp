0cc6e7d26eeea78864e16f70ac32e9acd36e2990
Gabi Melman <gmelman1@gmail.com>
2021-09-17 03:45:06 +0300

    Update test_cfg.cpp

--- tests/test_cfg.cpp
+++ tests/test_cfg.cpp
@@ -91,7 +91,7 @@ TEST_CASE("argv10", "[cfg]")
 
 TEST_CASE("argv11", "[cfg]")
 {
-    const char *argv[] = {};
+    const char *argv[] = nullptr;
     auto levels = load_argv_levels(0, argv);
     REQUIRE(levels.empty());
 }
