2bcef223840d3ffb44014d24a5ca724ee949c26c
Muhammed Galib Uludag <m231@tnctr.com>
2022-08-04 15:29:11 +0300

    Update common.h

--- include/spdlog/common.h
+++ include/spdlog/common.h
@@ -390,16 +390,17 @@ struct source_loc
 
 struct file_event_handlers
 {
-    std::function<void(const filename_t &filename)> before_open;
-    std::function<void(const filename_t &filename, std::FILE *file_stream)> after_open;
-    std::function<void(const filename_t &filename, std::FILE *file_stream)> before_close;
-    std::function<void(const filename_t &filename)> after_close;
-    file_event_handlers()
+        file_event_handlers()
         : before_open{nullptr}
         , after_open{nullptr}
         , before_close{nullptr}
         , after_close{nullptr}
     {}
+    
+    std::function<void(const filename_t &filename)> before_open;
+    std::function<void(const filename_t &filename, std::FILE *file_stream)> after_open;
+    std::function<void(const filename_t &filename, std::FILE *file_stream)> before_close;
+    std::function<void(const filename_t &filename)> after_close;
 };
 
 namespace details {
