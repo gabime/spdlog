e1c5dfecac2f5aca9cf4a60ecb10cc93e5c635e2
Muhammed Galib Uludag <m231@tnctr.com>
2022-08-04 15:24:46 +0300

    Update common.h

--- include/spdlog/common.h
+++ include/spdlog/common.h
@@ -44,16 +44,15 @@
 
 #include <spdlog/fmt/fmt.h>
 
-#ifndef SPDLOG_USE_STD_FORMAT
-#    if FMT_VERSION >= 80000 // backward compatibility with fmt versions older than 8
-#        define SPDLOG_FMT_RUNTIME(format_string) fmt::runtime(format_string)
-#        if defined(SPDLOG_WCHAR_FILENAMES) || defined(SPDLOG_WCHAR_TO_UTF8_SUPPORT)
-#            include <spdlog/fmt/xchar.h>
-#        endif
-#    else
-#        define SPDLOG_FMT_RUNTIME(format_string) format_string
-#        define SPDLOG_FMT_STRING(format_string) format_string
+#if !defined(SPDLOG_USE_STD_FORMAT) && FMT_VERSION >= 80000 // backward compatibility with fmt versions older than 8
+#    define SPDLOG_FMT_RUNTIME(format_string) fmt::runtime(format_string)
+#    define SPDLOG_FMT_STRING(format_string) FMT_STRING(format_string)
+#    if defined(SPDLOG_WCHAR_FILENAMES) || defined(SPDLOG_WCHAR_TO_UTF8_SUPPORT)
+#        include <spdlog/fmt/xchar.h>
 #    endif
+#else
+#    define SPDLOG_FMT_RUNTIME(format_string) format_string
+#    define SPDLOG_FMT_STRING(format_string) format_string
 #endif
 
 #ifdef __has_feature
