4accce5d7b57c268ea37e566a268e51a485b8581
gabime <gmelman1@gmail.comx>
2022-11-01 00:07:46 +0200

    Try again fixing fmt::vformat_to when SPDLOG_WCHAR_TO_UTF8_SUPPORT is defined

--- include/spdlog/logger.h
+++ include/spdlog/logger.h
@@ -398,7 +398,7 @@ protected:
             fmt_lib::vformat_to(
                 std::back_inserter(wbuf), fmt, fmt_lib::make_format_args<fmt_lib::wformat_context>(std::forward<Args>(args)...));
 #    else
-            fmt::vformat_to(fmt::appender(wbuf), fmt, fmt::make_format_args<fmt::wformat_context>(std::forward<Args>(args)...));
+            fmt::vformat_to(std::back_inserter(wbuf), fmt, fmt::make_format_args<fmt::wformat_context>(std::forward<Args>(args)...));
 #    endif
 
             memory_buf_t buf;
