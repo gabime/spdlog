a85ddc765a896a9e3e390eb4872da2f48ab2c803
M. Galib Uludag <m231@tnctr.com>
2023-03-26 22:20:25 +0200

    added source_location

--- include/spdlog/spdlog.h
+++ include/spdlog/spdlog.h
@@ -14,6 +14,7 @@
 #include <spdlog/logger.h>
 #include <spdlog/version.h>
 #include <spdlog/details/synchronous_factory.h>
+#include <spdlog/details/source_location.h>
 
 #include <chrono>
 #include <functional>
@@ -251,39 +252,39 @@ inline void critical(wformat_string_t<Args...> fmt, Args &&... args)
 #endif
 
 template<typename T>
-inline void trace(const T &msg)
+inline void trace(const T &msg, details::source_location loc = details::source_location::current())
 {
-    default_logger_raw()->trace(msg);
+    default_logger_raw()->trace(msg, source_loc{loc.file_name(), loc.line(), loc.function_name()});
 }
 
 template<typename T>
-inline void debug(const T &msg)
+inline void debug(const T &msg, details::source_location loc = details::source_location::current())
 {
-    default_logger_raw()->debug(msg);
+    default_logger_raw()->debug(msg, source_loc{loc.file_name(), loc.line(), loc.function_name()});
 }
 
 template<typename T>
-inline void info(const T &msg)
+inline void info(const T &msg, details::source_location loc = details::source_location::current())
 {
-    default_logger_raw()->info(msg);
+    default_logger_raw()->info(msg, source_loc{loc.file_name(), loc.line(), loc.function_name()});
 }
 
 template<typename T>
-inline void warn(const T &msg)
+inline void warn(const T &msg, details::source_location loc = details::source_location::current())
 {
-    default_logger_raw()->warn(msg);
+    default_logger_raw()->warn(msg, source_loc{loc.file_name(), loc.line(), loc.function_name()});
 }
 
 template<typename T>
-inline void error(const T &msg)
+inline void error(const T &msg, details::source_location loc = details::source_location::current())
 {
-    default_logger_raw()->error(msg);
+    default_logger_raw()->error(msg, source_loc{loc.file_name(), loc.line(), loc.function_name()});
 }
 
 template<typename T>
-inline void critical(const T &msg)
+inline void critical(const T &msg, details::source_location loc = details::source_location::current())
 {
-    default_logger_raw()->critical(msg);
+    default_logger_raw()->critical(msg, source_loc{loc.file_name(), loc.line(), loc.function_name()});
 }
 
 } // namespace spdlog
