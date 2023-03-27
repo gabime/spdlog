45f3b3c1c71fb640b0f0e61c9dcfa6fc6229257c
M. Galib Uludag <m231@tnctr.com>
2023-03-27 18:10:48 +0200

    fix msvc wchar_t ambiguity

--- include/spdlog/common.h
+++ include/spdlog/common.h
@@ -156,7 +156,7 @@ struct format_string_wrapper
         : fmt_{fmtstr}
         , loc_{loc}
     {}
-#if !defined(SPDLOG_USE_STD_FORMAT) && FMT_VERSION >= 80000
+#if SPDLOG_CPLUSPLUS > 201703L && !defined(SPDLOG_USE_STD_FORMAT) && FMT_VERSION >= 80000
     format_string_wrapper(fmt::basic_runtime<Char> fmtstr, details::source_location loc = details::source_location::current())
         : fmt_{fmtstr}
         , loc_{loc}
