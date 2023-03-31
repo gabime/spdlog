607a5217e55f4c43ed7ccd7ea83f93bcb1e52c35
H1X4 <10332146+H1X4Dev@users.noreply.github.com>
2023-03-31 20:06:03 +0300

    fix build of updated macro

--- include/spdlog/common.h
+++ include/spdlog/common.h
@@ -185,7 +185,7 @@ using fmt_runtime_string = fmt::basic_runtime<Char>;
 template<class T, class Char = char>
 struct is_convertible_to_basic_format_string
     : std::integral_constant<bool,
-          std::is_convertible<T, fmt::basic_string_view<Char>>::value || std::is_same<remove_cvref_t<T>, fmt_runtime_string>::value>
+          std::is_convertible<T, fmt::basic_string_view<Char>>::value || std::is_same<remove_cvref_t<T>, fmt_runtime_string<Char>>::value>
 {};
 
 #    if defined(SPDLOG_WCHAR_FILENAMES) || defined(SPDLOG_WCHAR_TO_UTF8_SUPPORT)
