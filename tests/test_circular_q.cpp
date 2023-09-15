fd91561734ca8f813eaed24f7d9a4abcd9d0b68c
Gabi Melman <gmelman1@gmail.com>
2023-09-16 02:17:06 +0300

    Update test_circular_q.cpp

--- tests/test_circular_q.cpp
+++ tests/test_circular_q.cpp
@@ -26,7 +26,7 @@ TEST_CASE("test_rolling", "[circular_q]")
 
     for (size_t i = 0; i < q_size + 2; i++)
     {
-        q.push_back(size_t{i}); // arg to push_back must be r value
+        q.push_back(std::move(i)); // arg to push_back must be r value
     }
 
     REQUIRE(q.size() == q_size);
