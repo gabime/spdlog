286218aeeb5c880d7c0e9528d645749e76a60fd1
gabime <gmelman1@gmail.com>
2023-10-02 01:40:59 +0300

    Removed state from default error handler

--- src/logger.cpp
+++ src/logger.cpp
@@ -85,29 +85,22 @@ bool logger::should_flush_(const details::log_msg &msg) {
     return (msg.log_level >= flush_level) && (msg.log_level != level::off);
 }
 
-void logger::err_handler_(const std::string &msg) {
-    if (custom_err_handler_) {
-        custom_err_handler_(msg);
-    } else {
-        using std::chrono::system_clock;
-        static std::mutex mutex;
-        static std::chrono::system_clock::time_point last_report_time;
-        static size_t err_counter = 0;
-        std::lock_guard<std::mutex> lk{mutex};
-        auto now = system_clock::now();
-        err_counter++;
-        if (now - last_report_time < std::chrono::seconds(1)) {
-            return;
-        }
-        last_report_time = now;
-        auto tm_time = details::os::localtime(system_clock::to_time_t(now));
-        char date_buf[64];
-        std::strftime(date_buf, sizeof(date_buf), "%Y-%m-%d %H:%M:%S", &tm_time);
+    void logger::err_handler_(const std::string &msg) {
+        if (custom_err_handler_) {
+            custom_err_handler_(msg);
+        } else {
+            using std::chrono::system_clock;
+            auto now = system_clock::now();
+            auto tm_time = details::os::localtime(system_clock::to_time_t(now));
+            char date_buf[64];
+            std::strftime(date_buf, sizeof(date_buf), "%Y-%m-%d %H:%M:%S", &tm_time);
 #if defined(USING_R) && defined(R_R_H)  // if in R environment
-        REprintf("[*** LOG ERROR #%04zu ***] [%s] [%s] %s\n", err_counter, date_buf, name().c_str(), msg.c_str());
+            REprintf("[*** LOG ERROR ***] [%s] [%s] %s\n", date_buf, name().c_str(),
+                 msg.c_str());
 #else
-        std::fprintf(stderr, "[*** LOG ERROR #%04zu ***] [%s] [%s] %s\n", err_counter, date_buf, name().c_str(), msg.c_str());
+            std::fprintf(stderr, "[*** LOG ERROR ***] [%s] [%s] %s\n", date_buf,
+                         name().c_str(), msg.c_str());
 #endif
+        }
     }
-}
 }  // namespace spdlog
