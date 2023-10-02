b1b6a229550dc4e6275f36df77f7c09e2be86232
Gabi Melman <gmelman1@gmail.com>
2023-10-02 20:20:44 +0300

    Update fmt.h

--- include/spdlog/fmt/fmt.h
+++ include/spdlog/fmt/fmt.h
@@ -10,12 +10,12 @@
 // By default, spdlog include its own copy.
 //
 
-#if defined(SPDLOG_USE_STD_FORMAT)  // SPDLOG_USE_STD_FORMAT is defined - use std::format
+#if defined(SPDLOG_USE_STD_FORMAT)  // use std::format
     #include <format>
-#elif !defined(SPDLOG_FMT_EXTERNAL)
-    #include "bundled/core.h"
-    #include "bundled/format.h"
-#else  // SPDLOG_FMT_EXTERNAL is defined - use external fmtlib
+#elif defined(SPDLOG_FMT_EXTERNAL)  // use external {fmt} lib
     #include <fmt/core.h>
     #include <fmt/format.h>
+#else  // use bundled {fmt} lib
+     #include "bundled/core.h"
+    #include "bundled/format.h"
 #endif
