f4204e05bdfac8e9c5f7e0e1411f2cdcfdd0fb6a
Gabi Melman <gmelman1@gmail.com>
2023-12-24 12:34:00 +0200

    Clean cmake

--- cmake/fmtlib.cmake
+++ cmake/fmtlib.cmake
@@ -1,10 +1,5 @@
 include(FetchContent)
 
-# Always build static fmtlib version
-set(BUILD_SHARED_LIBS_ORIG "${BUILD_SHARED_LIBS}")
-#set(BUILD_SHARED_LIBS OFF CACHE BOOL "Disable BUILD_SHARED_LIBS for fmtlib" FORCE)
-
-Set(FETCHCONTENT_QUIET FALSE)
 FetchContent_Declare(
         fmt
         GIT_REPOSITORY https://github.com/fmtlib/fmt.git
@@ -19,6 +14,3 @@ if(NOT fmt_POPULATED)
     set(FMT_OS OFF CACHE BOOL "Disable FMT_OS" FORCE)
     add_subdirectory(${fmt_SOURCE_DIR} ${fmt_BINARY_DIR})
 endif ()
-
-# Restore BUILD_SHARED_LIBS
-set(BUILD_SHARED_LIBS "${BUILD_SHARED_LIBS_ORIG}" CACHE BOOL "Restore BUILD_SHARED_LIBS" FORCE)
