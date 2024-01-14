fbd1ebcebf75cfc1576a9fda78ddb41fdb333b11
gabime <gmelman1@gmail.com>
2024-01-14 12:44:52 +0200

    Fixed format.sh script

--- scripts/format.sh
+++ scripts/format.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"/..
 pwd
-find_sources="find include src tests example bench -not ( -path include/spdlog/fmt/bundled -prune ) -type f -name *\.h -o -name *\.cpp"
+find_sources="find include src tests example bench -type f -name *\.h -o -name *\.cpp"
 echo -n "Running dos2unix     "
 $find_sources | xargs -I {} sh -c "dos2unix '{}' 2>/dev/null; echo -n '.'"
 echo
