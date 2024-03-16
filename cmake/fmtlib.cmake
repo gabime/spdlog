c923915805ab80ee45fde14c3cc94218fcd5808a
gabime <gmelman1@gmail.com>
2024-03-16 14:26:21 +0200

    DOWNLOAD_EXTRACT_TIMESTAMP FALSE when fetching fmt

--- cmake/fmtlib.cmake
+++ cmake/fmtlib.cmake
@@ -4,6 +4,7 @@ FetchContent_Declare(
         fmt
         URL https://github.com/fmtlib/fmt/archive/refs/tags/10.2.1.tar.gz
         URL_HASH SHA256=1250e4cc58bf06ee631567523f48848dc4596133e163f02615c97f78bab6c811
+        DOWNLOAD_EXTRACT_TIMESTAMP FALSE
 )
 
 FetchContent_GetProperties(fmt)
