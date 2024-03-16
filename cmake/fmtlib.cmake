a145ba62b860509b654151b3c64b28ffe942af38
gabime <gmelman1@gmail.com>
2024-03-16 15:07:18 +0200

    Fixed fmt cmake

--- cmake/fmtlib.cmake
+++ cmake/fmtlib.cmake
@@ -2,11 +2,9 @@ include(FetchContent)
 
 FetchContent_Declare(
         fmt
-        URL https://github.com/fmtlib/fmt/archive/refs/tags/10.2.1.tar.gz
-        URL_HASH SHA256=1250e4cc58bf06ee631567523f48848dc4596133e163f02615c97f78bab6c811
         DOWNLOAD_EXTRACT_TIMESTAMP FALSE
-)
-
+        URL https://github.com/fmtlib/fmt/archive/refs/tags/10.2.1.tar.gz
+        URL_HASH SHA256=1250e4cc58bf06ee631567523f48848dc4596133e163f02615c97f78bab6c811)
 FetchContent_GetProperties(fmt)
 if(NOT fmt_POPULATED)
     FetchContent_Populate(fmt)
