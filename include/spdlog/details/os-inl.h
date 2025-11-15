2a19a091b132f2c8b5e1899be22bd2731f731631
Jérémie Dumas <jedumas@adobe.com>
2025-11-15 08:31:41 -0800

    Fix UWP detection.

--- include/spdlog/details/os-inl.h
+++ include/spdlog/details/os-inl.h
@@ -567,7 +567,7 @@ SPDLOG_INLINE filename_t dir_name(const filename_t &path) {
     #pragma warning(disable : 4996)
 #endif  // _MSC_VER
 std::string SPDLOG_INLINE getenv(const char *field) {
-#if defined(_MSC_VER) && defined(__cplusplus_winrt)
+#if defined(_MSC_VER) && (WINAPI_FAMILY != WINAPI_FAMILY_DESKTOP_APP)
     return std::string{};  // not supported under uwp
 #else
     char *buf = std::getenv(field);
